aa1c5fff6c06bafe10921509dc87be5c6f08af7d
fulh <fulh@163.com>
2020-12-11 09:30:23 +0800

    test

---
+++ web/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+from io import BytesIO
+from utils.check_code import create_validate_code
+# Create your views here.
+
+
+def check_code(request):
+    """
+    验证码
+    :param request:
+    :return:
+    """
+    stream = BytesIO()
+    img, code = create_validate_code()
+    img.save(stream, 'PNG')
+    request.session['CheckCode'] = code
+    return HttpResponse(stream.getvalue())
+
+
+def longin(request):
+     if request.method=='GET':
+          return render(request,"login.html")
+     return HttpResponse("FULIHUA")
